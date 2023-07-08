1f1d89624f786a99d6884175a0d425c4ec72af11
Guillermo Lagos <guillermo.lagos@moofwd.com>
2023-07-07 22:39:55 -0400

    Added support to some network errors

--- app/src/main/java/guillermo/lagos/testfgm/data/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/guillermo/lagos/testfgm/data/remote/RemoteDataSourceImpl.kt
@@ -16,6 +16,7 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 class RemoteDataSourceImpl @Inject constructor(
@@ -23,17 +24,15 @@ class RemoteDataSourceImpl @Inject constructor(
     private val defaultUrl: String?,
     private val token: String?,
     private val company: String?
-): RemoteDataSource {
+) : RemoteDataSource {
     override suspend fun fetchStores(
         nextPage: String?
     ): Resource<Page> = try {
         if (
-            !defaultUrl.isNullOrEmpty()
-            &&
-            !token.isNullOrEmpty()
-            &&
+            !defaultUrl.isNullOrEmpty() &&
+            !token.isNullOrEmpty() &&
             !company.isNullOrEmpty()
-        ){
+        ) {
             val url = nextPage ?: defaultUrl
             val response: HttpResponse = client.get(url) {
                 headers {
@@ -43,15 +42,30 @@ class RemoteDataSourceImpl @Inject constructor(
                 contentType(ContentType.Application.Json)
                 parameter("per_page", 10)
             }
-            if (response.status == HttpStatusCode.OK) response.receive<DtoStoresResponse>()
-                .toPage()
-                .let {
-                    Log.e(this.javaClass.name, "URL: $url")
-                    Resource.Success(data = it)
-                }
-            else Resource.Error(exception = Exception("Server responded with status ${response.status.value}"))
-        } else Resource.Error(exception = Exception("Network parameters are missing"))
+            if (response.status == HttpStatusCode.OK) {
+                val dtoResponse = response.receive<DtoStoresResponse>()
+                val page = dtoResponse.toPage()
+                Log.e(this.javaClass.name, "URL: $url")
+                Resource.Success(data = page)
+            } else {
+                val errorMessage = getErrorMessage(response.status)
+                Resource.Error(exception = Exception(errorMessage))
+            }
+        } else {
+            Resource.Error(exception = Exception("Ups! Network parameters are missing"))
+        }
     } catch (e: ResponseException) {
         Resource.Error(exception = e)
+    } catch (e: UnknownHostException) {
+        Resource.Error(exception = Exception("Ups! Poor signal connection"))
+    }
+
+    private fun getErrorMessage(
+        status: HttpStatusCode
+    ) = when (status) {
+        HttpStatusCode.InternalServerError -> "Ups! Internal Server Error (500)"
+        HttpStatusCode.Forbidden -> "Ups! Forbidden Access (403)"
+        HttpStatusCode.NotFound -> "Ups! Resource Not Found (404)"
+        else -> "Ups! HTTP Error: ${status.value}"
     }
 }
